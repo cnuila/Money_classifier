1ed6d7155522aaf7cb8ef78596f419a4cd252eca
Carlos Nuila <cnuila14@icloud.com>
2021-06-17 10:25:23 -0600

    Reconociendo contorno billete

--- main.py
+++ main.py
@@ -2,47 +2,97 @@ import sys
 import cv2 as cv
 import numpy as np
 
-def preProcesamiento(img):
+def autoCanny(img):
+    medianaVector = np.median(img)
+    sigma = 0.33
+    lowerThre = int(max(0,(1.0 - sigma) * medianaVector))
+    upperThre = int(min(255,(1.0 - sigma) * medianaVector))
+    imgCanny = cv.Canny(img,lowerThre,upperThre)
+    return imgCanny
+
+#0 wide, 1 tight, 2 automatico con mediana
+def preProcesamiento(img,numCanny):
     imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    imgBlur = cv.GaussianBlur(imgGray,(5,5),1)    
-    imgCanny = cv.Canny(imgBlur,10,200)
+    imgBlur = cv.GaussianBlur(imgGray,(3,3),1) 
+   
+    if numCanny == 0:
+        imgCanny = cv.Canny(imgBlur,10,200)
+    elif numCanny == 1:
+        imgCanny = cv.Canny(imgBlur,255,250)
+    else:
+        imgCanny = autoCanny(imgBlur)
+
     kernel = np.ones((2,2))
     imgDial = cv.dilate(imgCanny,kernel,iterations=2)
-    imgThres = cv.erode(imgDial,kernel,iterations=1)    
-    return imgThres
+    imgErode = cv.erode(imgDial,kernel,iterations=1)
 
-def getContornoBillete(img):
+    return imgErode
+    
+
+def getContornosBillete(img):
     imgContornos = img.copy()
-    imgEdges = preProcesamiento(img)
-    contornos, jerarquia = cv.findContours(imgEdges,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE) 
-    areaMasGrande = 0
-    contornoMasGrande = 0
-    for contorno in contornos:
-        area = cv.contourArea(contorno) 
-        if area > areaMasGrande:
-            areaMasGrande = area
-            contornoMasGrande = contorno
 
-    #cv.drawContours(imgContornos,contornoMasGrande,-1,(0,255,0),3)
-    perimetro = cv.arcLength(contornoMasGrande,True)                
-    #aproximar esquinas
-    approx = cv.approxPolyDP(contornoMasGrande,0.02*perimetro, True)
+    areaMasAdecuada = 0
+    contornoMasAdecuado = 0
 
-    x, y, w, h = cv.boundingRect(approx)
-    #cv.rectangle(imgContornos,(x,y),(x+w,y+h),(255,0,0),2)                
-    alto = h + y
-    largo = w + x
+    altoImg,largoImg,canales = img.shape
+    for i in range(3):
+        imgEdges = preProcesamiento(img,i)
+        contornos, jerarquia = cv.findContours(imgEdges,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE) 
 
-    retVal = imgContornos[y:alto,x:largo]
+        for contorno in contornos:
+            perimetro = cv.arcLength(contorno,True)                
+
+            approxEsquinas = cv.approxPolyDP(contorno,0.02*perimetro, True)            
+
+            x, y, w, h = cv.boundingRect(approxEsquinas)
+            
+            areaEncontrada = w * h
+            areaTotal = altoImg * largoImg
+
+            porcentajeW = (w * 100) / largoImg
+            porcentajeH = (h * 100) / altoImg
+            porcentajeA = (areaEncontrada * 100) / areaTotal
+                    
+            if altoImg == 512:
+                if porcentajeA > 33 and porcentajeA < 83:
+                    if porcentajeW > 66 and porcentajeW <= 95:
+                        if porcentajeH > 21 and porcentajeH < 89:
+                            if areaMasAdecuada < areaEncontrada:
+                                contornoMasAdecuado = contorno
+                                areaMasAdecuada = areaEncontrada
+            elif largoImg == 512:
+                if porcentajeA > 37 and porcentajeA < 72:
+                    if porcentajeW > 45 and porcentajeW <= 100:
+                        if porcentajeH > 64 and porcentajeH < 95:
+                            if areaMasAdecuada < areaEncontrada:
+                                contornoMasAdecuado = contorno
+                                areaMasAdecuada = areaEncontrada
+        
+    #si no encuentra quitará un porcentaje alrededor de la foto
+    if areaMasAdecuada == 0:
+        if altoImg == 512:
+            quitarW = int(( 7 * largoImg ) / 100)
+            quitarH = int(( 20 * altoImg ) / 100)
+        elif largoImg == 512:
+            quitarW = int(( 20 * largoImg ) / 100)
+            quitarH = int(( 7 * altoImg ) / 100)
+
+        retVal = imgContornos[quitarH:altoImg-quitarH,quitarW:largoImg-quitarW]
+    else:
+        perimetro = cv.arcLength(contornoMasAdecuado,True)
+        approx = cv.approxPolyDP(contornoMasAdecuado,0.02*perimetro, True)
+        x, y, w, h = cv.boundingRect(approx) 
+        retVal = imgContornos[y:(h+y),x:(w+x)]                   
+
     return retVal
-
 
 def main(argv):
     foto = cv.imread(argv[0])
-    img = getContornoBillete(foto)
+    
+    img = getContornosBillete(foto)
     cv.imshow("IMG",img)
-    cv.waitKey(0)    
-
+    cv.waitKey(0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
